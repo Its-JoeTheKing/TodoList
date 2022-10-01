b395cd9d3c450c1293ddb82e7a8d867e008ceb20
Its-JoeTheKing <errfig22@gmail.com>
2022-10-01 15:04:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import {useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [task,setTaks] = useState('');
   return (
-    <div className="App">
+    <div className='App'>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
       <link href="https://fonts.googleapis.com/css2?family=Acme&family=Bangers&family=Bree+Serif&family=Josefin+Sans&family=Montserrat:wght@500&family=Poppins:wght@500&family=Roboto:wght@500&family=Secular+One&family=Signika+Negative&family=Staatliches&family=Teko&display=swap" rel="stylesheet"/>
-      <div >
-
+      <div className='Box'>
+        <h2 className='title'>Do It TodoList !!!</h2>
+        <input className='inpt' name='todo' value={task} onChange={(e)=>setTaks(e.target.value)} /><button className='add'>Add +</button>
       </div>
     </div>
   );
